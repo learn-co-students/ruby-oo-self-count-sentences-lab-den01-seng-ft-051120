4048d32e2ed329a36b16162360edf5f117f485a6
github username <github email address>
2020-05-14 09:31:29 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with? "."
   end
 
   def question?
-
+    self.end_with? "?"
   end
 
   def exclamation?
-
+    self.end_with? "!"
   end
 
   def count_sentences
-
+    split_sentences = self.split(/[|.|!|?]/)
+    scrubbed_sentences = split_sentences.delete_if { |sentence| sentence == ""}
+    scrubbed_sentences.length
   end
 end
