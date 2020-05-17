c5511e9a9b766c44090ef27920aa601a9058bb5b
Ben Rowe <brow495@gmail.com>
2020-05-17 10:24:11 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
-  def count_sentences
+  def punctuation
+    [".", "?", "!"].include? self 
+  end 
 
+
+  def count_sentences
+    count = self.chars.reduce({letters: 0, sentences: 0}) do |count, character|
+      if character.punctuation && count[:letters] > 1
+        count[:sentences] += 1
+        count[:letters] = 0
+      else 
+        count[:letters] += 1
+      end 
+      count 
+    end 
+    count[:sentences]
   end
 end
